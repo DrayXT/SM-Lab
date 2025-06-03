7c9deb7f67692f124e590de15227635f3ff177a6
Andrew <titaniumv22@gmail.com>
2025-06-03 19:16:34 +0700

    Add files via upload

---
+++ para_lab14/para_lab14/Form1.Designer.cs
@@ -0,0 +1,244 @@
+﻿namespace para_lab14
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            timer1 = new System.Windows.Forms.Timer(components);
+            bStart = new Button();
+            currentClientCountLabel = new Label();
+            currentTimeLabel = new Label();
+            bPause = new Button();
+            statisticsDataGridView = new DataGridView();
+            maxTimeStateLabel = new Label();
+            serviceIntensity = new NumericUpDown();
+            arrivalIntensity = new NumericUpDown();
+            label1 = new Label();
+            label2 = new Label();
+            lQueue = new Label();
+            maxQueueLengthLabel = new Label();
+            maxOperatorsLabel = new Label();
+            namea = new Label();
+            nOperators = new NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)statisticsDataGridView).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)serviceIntensity).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)arrivalIntensity).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)nOperators).BeginInit();
+            SuspendLayout();
+            // 
+            // timer1
+            // 
+            timer1.Tick += timer1_Tick;
+            // 
+            // bStart
+            // 
+            bStart.Location = new Point(344, 409);
+            bStart.Name = "bStart";
+            bStart.Size = new Size(94, 29);
+            bStart.TabIndex = 0;
+            bStart.Text = "Start";
+            bStart.UseVisualStyleBackColor = true;
+            bStart.Click += bStart_Click;
+            // 
+            // currentClientCountLabel
+            // 
+            currentClientCountLabel.AutoSize = true;
+            currentClientCountLabel.Location = new Point(12, 279);
+            currentClientCountLabel.Name = "currentClientCountLabel";
+            currentClientCountLabel.Size = new Size(50, 20);
+            currentClientCountLabel.TabIndex = 1;
+            currentClientCountLabel.Text = "label1";
+            // 
+            // currentTimeLabel
+            // 
+            currentTimeLabel.AutoSize = true;
+            currentTimeLabel.Location = new Point(320, 279);
+            currentTimeLabel.Name = "currentTimeLabel";
+            currentTimeLabel.Size = new Size(50, 20);
+            currentTimeLabel.TabIndex = 2;
+            currentTimeLabel.Text = "label2";
+            // 
+            // bPause
+            // 
+            bPause.Location = new Point(558, 409);
+            bPause.Name = "bPause";
+            bPause.Size = new Size(94, 29);
+            bPause.TabIndex = 3;
+            bPause.Text = "Pause";
+            bPause.UseVisualStyleBackColor = true;
+            bPause.Click += bPause_Click;
+            // 
+            // statisticsDataGridView
+            // 
+            statisticsDataGridView.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            statisticsDataGridView.Location = new Point(12, 12);
+            statisticsDataGridView.Name = "statisticsDataGridView";
+            statisticsDataGridView.RowHeadersWidth = 51;
+            statisticsDataGridView.Size = new Size(363, 246);
+            statisticsDataGridView.TabIndex = 4;
+            // 
+            // maxTimeStateLabel
+            // 
+            maxTimeStateLabel.AutoSize = true;
+            maxTimeStateLabel.Location = new Point(381, 12);
+            maxTimeStateLabel.Name = "maxTimeStateLabel";
+            maxTimeStateLabel.Size = new Size(185, 20);
+            maxTimeStateLabel.TabIndex = 5;
+            maxTimeStateLabel.Text = "Самое долгое состояние:";
+            // 
+            // serviceIntensity
+            // 
+            serviceIntensity.Location = new Point(613, 98);
+            serviceIntensity.Minimum = new decimal(new int[] { 1, 0, 0, 0 });
+            serviceIntensity.Name = "serviceIntensity";
+            serviceIntensity.Size = new Size(94, 27);
+            serviceIntensity.TabIndex = 6;
+            serviceIntensity.Value = new decimal(new int[] { 1, 0, 0, 0 });
+            // 
+            // arrivalIntensity
+            // 
+            arrivalIntensity.Location = new Point(613, 140);
+            arrivalIntensity.Minimum = new decimal(new int[] { 1, 0, 0, 0 });
+            arrivalIntensity.Name = "arrivalIntensity";
+            arrivalIntensity.Size = new Size(94, 27);
+            arrivalIntensity.TabIndex = 7;
+            arrivalIntensity.Value = new decimal(new int[] { 1, 0, 0, 0 });
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(381, 100);
+            label1.Name = "label1";
+            label1.Size = new Size(221, 20);
+            label1.TabIndex = 8;
+            label1.Text = "Интенсивность обслуживания";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(381, 142);
+            label2.Name = "label2";
+            label2.Size = new Size(189, 20);
+            label2.TabIndex = 9;
+            label2.Text = "Интенсивность прибытия";
+            // 
+            // lQueue
+            // 
+            lQueue.AutoSize = true;
+            lQueue.Location = new Point(12, 310);
+            lQueue.Name = "lQueue";
+            lQueue.Size = new Size(50, 20);
+            lQueue.TabIndex = 10;
+            lQueue.Text = "label3";
+            // 
+            // maxQueueLengthLabel
+            // 
+            maxQueueLengthLabel.AutoSize = true;
+            maxQueueLengthLabel.Location = new Point(381, 52);
+            maxQueueLengthLabel.Name = "maxQueueLengthLabel";
+            maxQueueLengthLabel.Size = new Size(159, 20);
+            maxQueueLengthLabel.TabIndex = 11;
+            maxQueueLengthLabel.Text = "Макс. длина очереди:";
+            // 
+            // maxOperatorsLabel
+            // 
+            maxOperatorsLabel.AutoSize = true;
+            maxOperatorsLabel.Location = new Point(381, 32);
+            maxOperatorsLabel.Name = "maxOperatorsLabel";
+            maxOperatorsLabel.Size = new Size(198, 20);
+            maxOperatorsLabel.TabIndex = 12;
+            maxOperatorsLabel.Text = "Макс. занятых операторов:";
+            // 
+            // namea
+            // 
+            namea.AutoSize = true;
+            namea.Location = new Point(381, 178);
+            namea.Name = "namea";
+            namea.Size = new Size(149, 20);
+            namea.TabIndex = 13;
+            namea.Text = "Кол-во операторов:";
+            // 
+            // nOperators
+            // 
+            nOperators.Location = new Point(613, 178);
+            nOperators.Minimum = new decimal(new int[] { 1, 0, 0, 0 });
+            nOperators.Name = "nOperators";
+            nOperators.Size = new Size(94, 27);
+            nOperators.TabIndex = 14;
+            nOperators.Value = new decimal(new int[] { 1, 0, 0, 0 });
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(nOperators);
+            Controls.Add(namea);
+            Controls.Add(maxOperatorsLabel);
+            Controls.Add(maxQueueLengthLabel);
+            Controls.Add(lQueue);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(arrivalIntensity);
+            Controls.Add(serviceIntensity);
+            Controls.Add(maxTimeStateLabel);
+            Controls.Add(statisticsDataGridView);
+            Controls.Add(bPause);
+            Controls.Add(currentTimeLabel);
+            Controls.Add(currentClientCountLabel);
+            Controls.Add(bStart);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)statisticsDataGridView).EndInit();
+            ((System.ComponentModel.ISupportInitialize)serviceIntensity).EndInit();
+            ((System.ComponentModel.ISupportInitialize)arrivalIntensity).EndInit();
+            ((System.ComponentModel.ISupportInitialize)nOperators).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private Button bStart;
+        private Label currentClientCountLabel;
+        private Label currentTimeLabel;
+        private Button bPause;
+        private DataGridView statisticsDataGridView;
+        private Label maxTimeStateLabel;
+        private NumericUpDown serviceIntensity;
+        private NumericUpDown arrivalIntensity;
+        private Label label1;
+        private Label label2;
+        private Label lQueue;
+        private Label maxQueueLengthLabel;
+        private Label maxOperatorsLabel;
+        private Label namea;
+        private NumericUpDown nOperators;
+    }
+}
---
+++ para_lab14/para_lab14/Form1.cs
@@ -0,0 +1,356 @@
+﻿using System.Diagnostics;
+using para_lab14;
+using System.Data;
+
+//var inputFlow = new InputFlow(new Bank(2));
+//inputFlow.newEvent(0);
+//Model.run(inputFlow.bank); // для ссылки пришлось сделать public bank
+
+namespace para_lab14
+{
+    public partial class Form1 : Form
+    {
+        private const double SIMULATION_MAX_TIME = 100.0; // Максимальное время симуляции
+
+        private InputFlow currentInputFlow; // Будем хранить текущий InputFlow
+
+        public Form1()
+        {
+            InitializeComponent();
+            timer1.Interval = 10;
+            serviceIntensity.Value = 2; // Устанавливаем начальное значение интенсивности обслуживания
+            arrivalIntensity.Value = 2; 
+            nOperators.Value = 3;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            // Выполняем один шаг симуляции
+            bool simulationRunning = Model.StepSimulation(SIMULATION_MAX_TIME);
+
+            if (!simulationRunning)
+            {
+                timer1.Stop();
+                Debug.WriteLine("Симуляция завершена.");
+                DisplayFinalStatistics(Model.GetFinalStatistics(), Model.Time);
+            }
+
+            currentClientCountLabel.Text = $"Кол-во клиентов: {Model.currentBankInstance.clientCount}";
+
+            lQueue.Text = $"Очередь: {Model.currentBankInstance.clientQueue.Count}";
+
+            currentTimeLabel.Text = $"Время: {Model.Time:F2}";
+        }
+
+        private void bStart_Click(object sender, EventArgs e)
+        {
+            // Очищаем DataGridView
+            statisticsDataGridView.DataSource = null;
+            statisticsDataGridView.Rows.Clear();
+            statisticsDataGridView.Columns.Clear();
+            maxTimeStateLabel.Text = "Самое долгое состояние:";
+            double serviceIn = (double)serviceIntensity.Value; // Получаем интенсивность обслуживания из NumericUpDown
+            double arrivalIn = (double)arrivalIntensity.Value;
+            int numberOfOperators = 3;
+            if (nOperators.Value > 0)
+                numberOfOperators = (int)nOperators.Value;
+
+            // Инициализация новой симуляции
+            Bank bank = new Bank(serviceIn, numberOfOperators);
+            currentInputFlow = new InputFlow(bank, arrivalIn);
+
+            Model.InitializeSimulation(bank); // Инициализируем модель
+            currentInputFlow.newEvent(0); // Запускаем первое событие прибытия
+
+            timer1.Start();
+        }
+
+        private void bPause_Click(object sender, EventArgs e)
+        {
+            if (timer1.Enabled)
+            {
+                timer1.Stop();
+                bPause.Text = "Resume";
+            }
+            else
+            {
+                timer1.Start();
+                bPause.Text = "Pause";
+            }
+        }
+        public void DisplayFinalStatistics(double[] statistics, double totalTime)
+        {
+            // Если этот метод будет вызван из другого потока (например, если симуляция в Task.Run),
+            // то потребуется Invoke. Но так как он вызывается из simulationTimer_Tick,
+            // который сам выполняется в UI-потоке, Invoke не нужен.
+            // Однако, это хорошая практика для методов, которые могут быть вызваны из разных потоков.
+            if (statisticsDataGridView.InvokeRequired)
+            {
+                statisticsDataGridView.Invoke(new Action(() => DisplayFinalStatistics(statistics, totalTime)));
+                return;
+            }
+
+            DataTable dt = new DataTable();
+            dt.Columns.Add("Кол-во клиентов", typeof(int));
+            dt.Columns.Add("Доля времени", typeof(double));
+            //dt.Columns.Add("Очередь (макс.)", typeof(int));
+            //dt.Columns.Add("Занято операторов (макс.)", typeof(int));
+
+            double maxProportion = 0.0;
+            int stateWithMaxProportion = -1;
+
+            for (int i = 0; i < statistics.Length; i++)
+            {
+                // Выводим только те строки, где есть какая-то статистика
+                if (statistics[i] > 0 || i < 5)
+                {
+                    dt.Rows.Add(i, statistics[i]);
+                }
+                // Находим максимальную долю времени
+                if (statistics[i] > maxProportion)
+                {
+                    maxProportion = statistics[i];
+                    stateWithMaxProportion = i;
+                }
+            }
+
+            statisticsDataGridView.DataSource = dt;
+
+            // форматирование столбца
+            if (statisticsDataGridView.Columns.Contains("Доля времени"))
+            {
+                statisticsDataGridView.Columns["Доля времени"].DefaultCellStyle.Format = "F4"; // 4 знака после запятой
+            }
+            statisticsDataGridView.AutoResizeColumns(DataGridViewAutoSizeColumnsMode.AllCells);
+
+            if (stateWithMaxProportion != -1)
+            {
+                maxTimeStateLabel.Text = $"Самое долгое состояние: кол-во клиентов {stateWithMaxProportion} ({maxProportion:P2})"; // P2 для процентного формата
+            }
+            else
+            {
+                maxTimeStateLabel.Text = "Самое долгое состояние: Нет данных";
+            }
+
+            maxQueueLengthLabel.Text = $"Макс. длина очереди: {Model.MaxQueueLengthRecorded}";
+            maxOperatorsLabel.Text = $"Макс. занятых операторов: {Model.MaxBusyOperatorsRecorded}";
+
+        }
+    }
+
+
+    class Model
+    {
+        static public double Time { get; set; } = 0.0;
+        public static Random random = new Random();
+        static double[] statistics = new double[500];
+        internal static Bank currentBankInstance;
+        public static int MaxQueueLengthRecorded { get; private set; }
+        public static int MaxBusyOperatorsRecorded { get; private set; }
+
+        public static double getRND()
+        {
+            return random.NextDouble();
+        }
+        
+        static PriorityQueue<Event, double> eventQueue = new PriorityQueue<Event, double>();
+
+        internal static void NewEvent(Agent agent, double time)
+        {
+            Event e = new Event(agent, time);
+            eventQueue.Enqueue(e, time);
+        }
+
+        internal static void InitializeSimulation(Bank bankInstance)
+        {
+            Time = 0;
+            MaxQueueLengthRecorded = 0;
+            MaxBusyOperatorsRecorded = 0;
+            // Очищаем очередь событий и статистику для новой симуляции
+            eventQueue.Clear();
+            for (int i = 0; i < statistics.Length; i++)
+            {
+                statistics[i] = 0;
+            }
+            currentBankInstance = bankInstance; // Сохраняем ссылку на банк
+        }
+
+        internal static bool StepSimulation(double maxTime)
+        {
+            if (Time >= maxTime) // Условие завершения симуляции
+            {
+                if (Time > 0) // Избегаем деления на ноль, если симуляция не началась
+                {
+                    for (int i = 0; i < statistics.Length; i++)
+                    {
+                        statistics[i] /= Time; // Нормализуем статистику
+                    }
+                }
+                return false; // Симуляция завершена
+            }
+
+            if (eventQueue.Count == 0) // Если событий больше нет, но время еще не вышло
+            {
+                Debug.WriteLine("Очередь событий пуста до достижения максимального времени.");
+                return false;
+            }
+
+            var e = eventQueue.Dequeue();
+            Debug.WriteLine(e.agent + " : " + e.time);
+
+            // Накапливаем статистику
+            statistics[currentBankInstance.clientCount] += e.time - Time;
+            e.agent.processEvent(e);
+            Time = e.time;
+
+            MaxQueueLengthRecorded = Math.Max(MaxQueueLengthRecorded, currentBankInstance.clientQueue.Count);
+            MaxBusyOperatorsRecorded = Math.Max(MaxBusyOperatorsRecorded, currentBankInstance.busyOperators);
+
+            return true; // Симуляция продолжается
+        }
+        public static double[] GetFinalStatistics()
+        {
+            return statistics;
+        }
+
+        /*internal static void run(Bank bankInstance) // добавили ссылку на банк
+        {
+            Time = 0;
+            while (Time < 100)
+            {
+                var e = eventQueue.Dequeue();
+                Debug.WriteLine(e.agent + " : " + e.time);
+                statistics[bankInstance.clientCount] += e.time - Time; // ссылка на банк есть
+                e.agent.processEvent(e);
+                Time = e.time;
+            }
+
+            for (int i = 0; i < statistics.Count(); i++)
+            {
+                //if (statistics[i] != 0)
+                //{
+                    statistics[i] /= Time;
+                    Debug.WriteLine($"Клиентов {i} - {statistics[i]}");
+                //}
+            }
+        }*/
+    }
+
+    class Event
+    {
+        public Event(Agent agent, double time)
+        {
+            this.time = time;
+            this.agent = agent;
+        }  
+        public double time { get; set; }
+        public Agent agent { get; set; }
+    }
+
+    abstract class Agent
+    {
+        //public abstract string Name { get; }
+        public abstract void processEvent(Event e);
+    }
+
+    class InputFlow : Agent
+    {
+        public Bank bank { get; set; } // пришлось сделать public
+        public double intensity { get; private set; }
+
+        public InputFlow(Bank bank, double arrivalIntensity)
+        {
+            this.bank = bank;
+            this.intensity = arrivalIntensity;
+        }
+
+        private double generateNextArrival()
+        {
+            //return -Math.Log(Model.random.NextDouble()) / intensity; // две точки нехорошо
+            return -Math.Log(Model.getRND()) / intensity;
+        }
+        public override void processEvent(Event e)
+        {
+            bank.newClient(e.time);
+            newEvent(e.time);
+        }
+        public void newEvent(double time)
+        {
+            Model.NewEvent(this, time + generateNextArrival());
+        }
+    }
+
+    class Bank : Agent
+    {
+        public int clientCount { get; set; }
+        public double intensity { get; private set; }
+        public int totalOperators { get; private set; }
+        public int busyOperators { get; private set; }
+
+        // Очередь для клиентов, которые ждут свободного оператора
+        // Мы не храним сами объекты клиентов, так как они не имеют состояния,
+        // но можем использовать int для отслеживания их количества
+        public Queue<int> clientQueue;
+
+        public Bank (double intensity, int totalOperators)
+        {
+            this.intensity = intensity;
+            this.totalOperators = totalOperators;
+            this.busyOperators = 0; // Изначально все операторы свободны
+            this.clientCount = 0; // Изначально клиентов нет
+            this.clientQueue = new Queue<int>(); // Инициализация очереди
+        }
+        public override void processEvent(Event e)
+        {
+            // Событие ProcessEvent означает, что оператор закончил обслуживание клиента
+            busyOperators--; // Освобождаем оператора
+            clientCount--; // Уменьшаем общее количество клиентов в банке
+            Debug.WriteLine($"Время: {e.time:F2}, Клиент обслужен. Занято операторов: {busyOperators}, В очереди: {clientQueue.Count}");
+
+            //if (clientCount > 0) newEvent(e.time);
+
+            if (clientQueue.Count > 0 && busyOperators < totalOperators)
+            {
+                clientQueue.Dequeue(); // Удаляем клиента из очереди
+                busyOperators++; // Занимаем оператора
+                newEvent(e.time); // Планируем новое событие обслуживания для этого клиента
+            }
+        }
+        
+        internal void newClient(double arrivalTime)
+        {
+            /*if (clientCount == 0)
+            {
+                clientCount++;
+                newEvent(time);
+            }
+            else clientCount++;*/
+
+            clientCount++; // Увеличиваем общее количество клиентов в банке
+
+            Debug.WriteLine($"Время: {arrivalTime:F2}, Новый клиент прибыл. Всего клиентов: {clientCount}");
+
+            // Если есть свободный оператор
+            if (busyOperators < totalOperators)
+            {
+                busyOperators++; // Занимаем оператора
+                newEvent(arrivalTime); // Планируем событие завершения обслуживания для этого клиента
+            }
+            else
+            {
+                clientQueue.Enqueue(1); // Добавляем "единицу" в очередь, чтобы просто отметить наличие клиента
+                Debug.WriteLine($"Время: {arrivalTime:F2}, Клиент встал в очередь. Текущая очередь: {clientQueue.Count}");
+            }
+        }
+        public void newEvent(double time)
+        {
+            Model.NewEvent(this, time + generateNextArrival());
+        }
+        private double generateNextArrival()
+        {
+            //return -Math.Log(Model.random.NextDouble()) / intensity; // две точки нехорошо
+            return -Math.Log(Model.getRND()) / intensity;
+        }
+    }
+}
+
